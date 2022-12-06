1defe9403ee1053e2995b8e939dffda0cca71e20
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2022-12-06 12:38:18 +0300

    Kafka forced startup

--- start/kafka.sh
+++ start/kafka.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # Starting standalone Kafka
 
-while [[ "$( ps -ef | grep -i zookeeper | grep -v grep | wc -l | tr -d '\n' )" != 1 ]]; do 
+while [[ "$( ps -ewwo cmd | grep -i zookeeper | grep -v grep | wc -l | tr -d '\n' )" != 1 ]]; do 
     /home/hsk/zookeeper/bin/zkServer.sh start
     sleep 1
 done
 
-/home/hsk/kafka/bin/kafka-server-start.sh -daemon /home/hsk/kafka/config/server.properties
+while [[ "$( /home/hsk/kafka/bin/kafka-broker-api-versions.sh --bootstrap-server localhost:9092 | grep id | grep -v WARN | wc -l | tr -d '\n' )" != 1 ]]; do 
+    /home/hsk/kafka/bin/kafka-server-start.sh -daemon /home/hsk/kafka/config/server.properties
+    sleep 1
+done
